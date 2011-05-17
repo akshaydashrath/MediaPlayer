85512e231a19e124e76b173c4b5aaf1356b3b185
akshaydashrath <akshaydashrath@gmail.com>
2011-05-17 13:28:43 -0400

    -Added seekbar to play music at a particular position

--- src/com/akshay/mp3player/Player.java
+++ src/com/akshay/mp3player/Player.java
@@ -4,28 +4,20 @@ import java.io.File;
 import java.io.FileInputStream;
 
 import android.app.Activity;
-import android.content.ContentProviderOperation;
-import android.content.ContentProviderOperation.Builder;
-import android.content.ContentValues;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.database.Cursor;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.ToggleButton;
 
 public class Player extends Activity implements Runnable {
 	private MediaPlayer mp;
-	private ProgressBar progressBar;
+	private SeekBar progressBar;
 	private ToggleButton button;
 
 	/** Called when the activity is first created. */
@@ -33,22 +25,38 @@ public class Player extends Activity implements Runnable {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		progressBar = (SeekBar) findViewById(R.id.progress);
+		button = (ToggleButton) findViewById(R.id.start_stop);
+		
+		
 		initMediaPlayer();
 		File mp3 = getMP3File("/test.mp3");
 		setFileForPlayBack(mp3);
 
-		progressBar = (ProgressBar) findViewById(R.id.progress);
-		button = (ToggleButton) findViewById(R.id.start_stop);
+		
 		button.setOnCheckedChangeListener(new OnCheckedChangeListener(){
 			@Override
 			public void onCheckedChanged(CompoundButton button, boolean isChecked) {
 				if (isChecked){
-					Log.i("Akshay", "is for start checked " + isChecked);
 					startPlayback();
 				} else {
-					Log.i("Akshay", "is for pause checked " + isChecked);
 					pausePlayback();
 				}
+			}			
+		});
+		
+		progressBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				if (fromUser){
+					mp.seekTo(progress);
+				}
+			}
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {				
+			}
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {				
 			}			
 		});
 	}
@@ -62,6 +70,8 @@ public class Player extends Activity implements Runnable {
 			FileInputStream fis = new FileInputStream(mp3);
 			mp.setDataSource(fis.getFD());
 			mp.prepare();
+			progressBar.setProgress(0);
+			progressBar.setMax(mp.getDuration());
 		} catch (Exception e){
 			Log.e("Akshay", e.getMessage());
 		} finally {
@@ -78,8 +88,6 @@ public class Player extends Activity implements Runnable {
 
 	private void startPlayback(){
 		if(mp != null && mp.isPlaying()) return;
-		progressBar.setProgress(0);
-		progressBar.setMax(mp.getDuration());
 		mp.start();                      
 		new Thread(Player.this).start();
 	}
@@ -99,18 +107,18 @@ public class Player extends Activity implements Runnable {
 
 	@Override
 	public void run() {
-		int CurrentPosition= 0;
+		int currentPosition= 0;
 		int total = mp.getDuration();
-		while(mp!=null && CurrentPosition<total){
+		while(mp!=null && currentPosition<total){
 			try {
 				Thread.sleep(1000);
-				CurrentPosition= mp.getCurrentPosition();
+				currentPosition= mp.getCurrentPosition();
 			} catch (InterruptedException e) {
 				return;
 			} catch (Exception e){
 				return;
 			}            
-			progressBar.setProgress(CurrentPosition);
+			progressBar.setProgress(currentPosition);
 		}
 	}
 
